d5d421dff9bfdc7868bc08e25e217e0e13110bf4
unknown <josealvarez1654@outlook.com>
2023-08-16 15:03:31 -0500

    responsive

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ const contenido = [
   const busquedaInput = document.getElementById('busqueda');
   const contenidoFiltradoDiv = document.getElementById('contenedorFiltrado');
   
-  // probando
+  // probando 2.0
   function filtrarContenido() {
     
     const tipoSeleccionado = tipoSelect.value;
