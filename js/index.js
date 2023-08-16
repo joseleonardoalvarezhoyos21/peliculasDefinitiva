2d2347a87678e52a79a2f4fa54dc128963ba918c
unknown <josealvarez1654@outlook.com>
2023-08-16 15:20:01 -0500

    incorporacion del trailer

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ const contenido = [
   const busquedaInput = document.getElementById('busqueda');
   const contenidoFiltradoDiv = document.getElementById('contenedorFiltrado');
   
-  // probando 4.0
+  // probando 6.0
   function filtrarContenido() {
     
     const tipoSeleccionado = tipoSelect.value;
