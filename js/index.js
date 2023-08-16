6466dbac2128bc7d1b73668096b0bc3305ba1027
unknown <josealvarez1654@outlook.com>
2023-08-16 15:14:12 -0500

    categorias terminadas

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ const contenido = [
   const busquedaInput = document.getElementById('busqueda');
   const contenidoFiltradoDiv = document.getElementById('contenedorFiltrado');
   
-  // probando 3.0
+  // probando 4.0
   function filtrarContenido() {
     
     const tipoSeleccionado = tipoSelect.value;
